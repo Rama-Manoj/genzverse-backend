56cf2825abb60eb410c1b098af3aed3df4fb464f
Rama Manoj <146643140+Rama-Manoj@users.noreply.github.com>
2026-06-01 19:11:31 +0530

    Update GenzverseApplication.java

--- src/main/java/com/genzverse/GenzverseApplication.java
+++ src/main/java/com/genzverse/GenzverseApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableAsync	
 public class GenzverseApplication {
 
 	public static void main(String[] args) {
