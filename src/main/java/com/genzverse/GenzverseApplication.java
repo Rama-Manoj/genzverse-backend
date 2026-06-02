ea7ea208fe1c0fd431fa8961d08c63bda1fd50ea
Rama Manoj <146643140+Rama-Manoj@users.noreply.github.com>
2026-06-02 10:57:09 +0530

    Enable asynchronous processing in GenzverseApplication

--- src/main/java/com/genzverse/GenzverseApplication.java
+++ src/main/java/com/genzverse/GenzverseApplication.java
@@ -2,7 +2,7 @@ package com.genzverse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync	
