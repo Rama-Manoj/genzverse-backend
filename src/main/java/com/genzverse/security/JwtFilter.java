b53d4539c5cd0392c3aa6096471d6cb499046906
Rama Manoj <146643140+Rama-Manoj@users.noreply.github.com>
2026-06-13 11:28:03 +0530

    Update JwtFilter.java

--- src/main/java/com/genzverse/security/JwtFilter.java
+++ src/main/java/com/genzverse/security/JwtFilter.java
@@ -38,6 +38,10 @@ public class JwtFilter extends OncePerRequestFilter
             FilterChain filterChain)
             throws ServletException, IOException
     {
+                if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+                    filterChain.doFilter(request, response);
+                    return;
+                }
     	
     			String path = request.getServletPath();
 
