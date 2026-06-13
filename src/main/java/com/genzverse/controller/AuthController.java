5f3b4cc5557bdae215420ab5c3b5d9e4fa62f1a8
Rama Manoj <146643140+Rama-Manoj@users.noreply.github.com>
2026-06-13 09:35:51 +0530

    Update AuthController.java

--- src/main/java/com/genzverse/controller/AuthController.java
+++ src/main/java/com/genzverse/controller/AuthController.java
@@ -23,11 +23,25 @@ import jakarta.validation.Valid;
 @RequestMapping("/api/auth")
 public class AuthController 
 {
+    private final EmailService emailService;
     private final AuthService authService;
 
-    public AuthController(AuthService authService)
+    public AuthController( AuthService authService, EmailService emailService)
     {
         this.authService = authService;
+        this.emailService = emailService;
+    }
+
+    @GetMapping("/test-email")
+    public String testEmail()
+    {
+        emailService.sendEmail(
+                "yourgmail@gmail.com",
+                "GenZVerse Test",
+                "Email service is working"
+        );
+    
+        return "Triggered";
     }
 
     @PostMapping("/register")
